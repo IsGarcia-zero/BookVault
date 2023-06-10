58ae381dbbd806b41b093fd38eaeb4d687bf7fd3
SamuelBarush <soviedor1700@alumno.ipn.mx>
2023-06-10 17:40:18 -0600

    Retroalimentacion Usuarios, Administrador y UPDATE Solicitud de Libros

---
+++ scripts/nav.js
@@ -0,0 +1,8 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+    var navbar = document.getElementById('navbar');
+    if (navbar.style.display === "none") {
+        navbar.style.display = "flex";
+    } else {
+        navbar.style.display = "none";
+    }
+});
