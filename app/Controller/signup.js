385872fdb7ca9abfb1e72da8b185f772286e0760
Diego Chavez <diegochavor@gmail.com>
2023-06-15 12:03:43 -0600

    version beta del login, aun falta autentificar

---
+++ app/Controller/signup.js
@@ -0,0 +1,15 @@
+const Signup = document.querySelector("#signup-form");
+Signup.addEventListener("submit", (e) =>{
+                                        e.preventDefault(); console.log("enviando")
+                                        const SignupEmail = document.querySelector("#email").value;
+                                        const SignupPass = document.querySelector("#pass").value;
+                                        const SignupNombre = document.querySelector("#nombre").value;
+                                        const SignupEdad = document.querySelector("#edad").value;
+                                        console.log(SignupEdad,SignupEmail,SignupPass,SignupNombre)
+                                        
+                                        auth
+                                            .createUserwithEmailandPassword(SignupEmail,SignupPass).then(userCredential =>{
+                                            console.log("signedUp")
+                                        })
+                                    
+                                    })
