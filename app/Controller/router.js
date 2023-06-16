772b4e35847166a276d1d9508585fb6dd9f1fda6
unknown <johamamo2022@gmail.com>
2023-06-15 23:13:39 -0600

    Cambios en el lector y insert para tabla de favoritos

--- app/Controller/router.js
+++ app/Controller/router.js
@@ -1,45 +1,108 @@
-const express = require('express');
-const router = express.Router();
-const conexion = require('../Model/database/db');
-
-router.get('/inicio', (req, res) => {
-    // Primera consulta
-    conexion.query('SELECT  * FROM LIBRO', (error, results1) => {
-        if (error) {
-            throw error;
-        } else {
-            // Segunda consulta
-            conexion.query('SELECT DISTINCT genero FROM LIBRO', (error, results2) => {
-                if (error) {
-                    throw error;
-                } else {
-                    conexion.query('SELECT COUNT(*) AS cantidad FROM (SELECT DISTINCT genero FROM LIBRO) AS subquery;', (error, results3) => {
-                        if (error) {
-                            throw error;
-                        } else {
-                            res.render('mostrarlibros', { results1: results1, results2: results2, results3: results3 });
-                        }
-                    });
-                }
-            });
-        }
-    });
+var express = require('express');
+var router = express.Router();
+var connection = require('../model/js/database');
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('login', { title: 'Express' });
+});
+router.get('/signup', (req, res, next) =>{
+  res.render('signup', { title: 'Express' });
+});
+router.get('/config', (req, res, next) =>{
+  res.render('indexAdmin', { title: 'Express' });
+});
+router.get('/bandeja', (req, res, next) =>{
+  res.render('bandejaEntrada', { title: 'Express' });
+});
+router.get('/busqueda', (req, res, next) =>{
+  res.render('busquedaNoUsuario', { title: 'Express' });
+});
+router.get('/favoritos', (req, res, next) =>{
+  res.render('favoritos', { title: 'Express' });
+});
+router.get('/lector', (req, res, next) =>{
+  res.render('lector', { title: 'Express' });
+});
+router.get('/retroalimentacion', (req, res, next) =>{
+  res.render('retroalimentacionUsuario', { title: 'Express' });
+});
+router.get('/solicitud', (req, res, next) =>{
+  res.render('solicitud_libros', { title: 'Express' });
 });
 
-router.use(express.json());
-
-router.post('/inicio', (req, res) => {
-  const consulta = req.body.consulta;
-  conexion.query("SELECT * FROM LIBRO WHERE nombreAutor LIKE '%{}%'".format(consulta), (error, resultados) => {
-    if (error) {
-      console.error('Error al ejecutar la consulta:', error);
-      res.status(500).json({ error: 'Error al ejecutar la consulta' });
+router.get('/subirLibros', (req, res, next) =>{
+  res.render('subirLibrosAdmin', { title: 'Express' });
+});
+router.get('/home', function(req, res, next) {
+  connection.query('SELECT * FROM LIBRO',(error, results1, fields)=>{
+    if(error){
+      console.log(error);
+      throw error;
+    }else{
+      console.log(results1);
+      connection.query('SELECT DISTINCT genero FROM LIBRO',(error, results2, fields)=>{
+        if(error){
+          console.log(error);
+          throw error;
+        }else{
+          connection.query('SELECT COUNT(*) AS cantidad FROM (SELECT DISTINCT genero FROM LIBRO) AS subquery;',(error, results3, fields)=>{
+            if(error){
+              console.log(error);
+              throw error;
+            }else{
+              console.log(results3);
+              res.render('index', { title: 'Express', results1: results1, results2: results2, results3: results3 });
+            }
+          });
+        }
+      });
+    }
+  });
+  //res.render('index', { title: 'Express' });
+});
+router.get('/getData', function(req, res, next) {
+  let searchQuery = req.query.searchQuery;
+  let query = `SELECT titulo FROM LIBRO WHERE titulo LIKE ? LIMIT 3`;
+  connection.query(query, [`%${searchQuery}%`], (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'An error occurred' });
     } else {
-      res.json(resultados);
+      res.json(data);
     }
   });
 });
 
+const express = require('express');
 
 
+router.use(express.json()); // Middleware para procesar solicitudes con cuerpo JSON
+router.use(express.urlencoded({ extended: true })); // Middleware para procesar solicitudes con datos en formato URL-encoded
+
+router.post('/lector', (req, res) => {
+  // Obtener los datos enviados desde el cliente
+  const { nombreAutor, fecha, tituloLibro, usuarioId, libroId } = req.body;
+
+  const libFavorito = {
+    nombreAutor,
+    fecha,
+    tituloLibro,
+    USUARIO_idUsuario: usuarioId,
+    LIBRO_idLibro: libroId
+  };
+
+  connection.query('INSERT INTO favorito SET ?', libFavorito, (error, results) => {
+    if (error) {
+      console.error('Error al insertar en la base de datos:', error);
+      res.status(500).json({ error: 'Error al insertar en la base de datos' });
+    } else {
+      console.log('Registro insertado correctamente en la base de datos');
+      res.json({ message: 'Favorito marcado correctamente' });
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Servidor escuchando en el puerto 3000');
+});
 module.exports = router;
