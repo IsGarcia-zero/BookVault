cd65255bc9ada4e413a1ed9b12fb6dc334e8e86f
Diego Chavez <diegochavor@gmail.com>
2023-06-22 11:25:47 -0600

    login sin lorem

--- routes/index.js
+++ routes/index.js
@@ -66,7 +66,7 @@ router.post('/signup', (req, res) => {
   const idUsuario = uuid.v4();
   const emailRegex = /^[A-Za-z0-9._%+-]+@(alumno\.ipn\.mx|ipn\.mx)$/;
   // Verificar si el usuario ya existe en la base de datos
-  connection.query('SELECT * FROM usuario WHERE correo = ?', [email], (error, results, fields) => {
+  connection.query('SELECT * FROM usuarios WHERE correo = ?', [email], (error, results, fields) => {
     if (error) {
       console.log(error);
       throw error;
